75f322025fe20038f5a131499d34cbe768e4f1ea
VinceCDA <vincent.hitman47@hotmail.fr>
2022-01-14 15:26:18 +0100

    Added sync between player and restrict camera angles

--- FPS_Multi_Project_1/Assets/Scripts/PlayerController.cs
+++ FPS_Multi_Project_1/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
+
 public class PlayerController : MonoBehaviour
 {
+    #region Variables globales
     [SerializeField]
     private float _vitesse = 3.0f;
 
     [SerializeField]
     private float _sensibiliteSourisX = 3.0f;
+
     [SerializeField]
     private float _sensibiliteSourisY = 3.0f;
 
+    [SerializeField]
+    private float _puissanceJetpack = 1000.0f;
 
     private PlayerMotor _motor;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +29,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CalculVelocite();
-        CalculRotation();
-        CalculRotationCamera();
+        CalculRotationHorizontal();
+        CalculRotationVertical();
+        CalculForceJetPack();
     }
 
     // Calcul la vélocité du joueur
@@ -40,10 +47,9 @@ public class PlayerController : MonoBehaviour
         Vector3 velocite = (deplacementVertical + deplacementHorizontal).normalized * _vitesse;
         _motor.Deplacer(velocite);
 
-        Debug.Log(velocite);
     }
-    // Calcul rotation du joueur
-    private void CalculRotation()
+    // Calcul rotation horizontal du joueur
+    private void CalculRotationHorizontal()
     {
         float yRotation = Input.GetAxisRaw("Mouse X");
        
@@ -52,8 +58,8 @@ public class PlayerController : MonoBehaviour
 
         _motor.Tourner(rotation);
     }
-    // Calcul rotation de la camera
-    private void CalculRotationCamera()
+    // Calcul rotation vertical de la camera
+    private void CalculRotationVertical()
     {
         
         float xRotation = Input.GetAxisRaw("Mouse Y");
@@ -61,5 +67,15 @@ public class PlayerController : MonoBehaviour
         Vector3 rotation = new Vector3(xRotation, 0, 0) * _sensibiliteSourisY;
 
         _motor.TournerCamera(rotation);
+    }
+    // Calcul de la force du Jetpack
+    private void CalculForceJetPack()
+    {
+        Vector3 forceJetpack = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            forceJetpack = Vector3.up * _puissanceJetpack;
+        }
+        _motor.UtiliserJetpack(forceJetpack);
     }
 }
--- FPS_Multi_Project_1/Assets/Scripts/PlayerMotor.cs
+++ FPS_Multi_Project_1/Assets/Scripts/PlayerMotor.cs
@@ -1,28 +1,48 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ConfigurableJoint))]
 public class PlayerMotor : MonoBehaviour
 {
+    #region Variables globales
     [SerializeField]
     private Camera _camera;
 
+    [SerializeField]
+    private float _angleCameraCourante = 0.0f;
     [SerializeField]
     private Vector3 _velocite;
 
     [SerializeField]
     private Vector3 _rotation;
+    [SerializeField]
+    private float _limiteRotationCamera = 85.0f;
 
     [SerializeField]
     private Vector3 _rotationCamera;
 
+    [SerializeField]
+    private Vector3 _velociteJetpack;
+
+    [Header("Joint Option")]
+    [SerializeField]
+    private float _jointYDriveSpring = 20.0f;
+
+    [SerializeField]
+    private float _jointYDriveMax = 60.0f;
 
     private Rigidbody _rigidbody;
+    private ConfigurableJoint _joint;
+    private JointDrive _yJointDrive;
+    #endregion
 
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _joint = GetComponent<ConfigurableJoint>();
+        _yJointDrive = _joint.yDrive;
     }
 
     // Update is called once per frame
@@ -36,6 +56,9 @@ public class PlayerMotor : MonoBehaviour
         EffectuerDeplacer();
         EffectuerTourner();
     }
+
+
+    #region Recuperation des valeurs de PlayerController
     public void Deplacer(Vector3 velocite)
     {
         _velocite = velocite;
@@ -48,16 +71,45 @@ public class PlayerMotor : MonoBehaviour
     {
         _rotationCamera = rotationCamera;
     }
+    public void UtiliserJetpack(Vector3 forceJetpack)
+    {
+        _velociteJetpack = forceJetpack;
+    }
+    #endregion
+
+
+    // Deplacement effectif du joueur
     private void EffectuerDeplacer()
     {
         if (_velocite != Vector3.zero)
         {
             _rigidbody.MovePosition(_rigidbody.position + _velocite * Time.deltaTime);
         }
+        if (_velociteJetpack != Vector3.zero)
+        {
+            _rigidbody.AddForce(_velociteJetpack * Time.deltaTime, ForceMode.Acceleration);
+            _yJointDrive.positionSpring = 0.0f;
+            _joint.yDrive = _yJointDrive;
+        }
+        else
+        {
+            _yJointDrive.positionSpring = _jointYDriveSpring;
+            _joint.yDrive = _yJointDrive;
+        }
     }
+    // Rotation effective du joueur
     private void EffectuerTourner()
     {
         _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(_rotation));
-        _camera.transform.Rotate(-_rotationCamera);
+        RestreindreAngleCamera();
+        _camera.transform.localEulerAngles = -_rotationCamera;
+
+    }
+    //Restriction de l'angle vertical du joueur
+    private void RestreindreAngleCamera()
+    {
+        _angleCameraCourante += _rotationCamera.x;
+        _angleCameraCourante = Mathf.Clamp(_angleCameraCourante, -_limiteRotationCamera, _limiteRotationCamera);
+        _rotationCamera.x = _angleCameraCourante;
     }
 }
